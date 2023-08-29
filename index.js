c389986e38397ee5fe198e7534e7c83390a27dd5
AdityaU74 <upadhyay.aditya405@gmail.com>
2023-08-30 00:42:29 +0530

    commit

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ const bucket = 'adi-app';
 // }
 app.use(express.json());
 app.use(cookieParser());
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/uploads', express.static(__dirname+'/uploads'));
 // app.use(cors({
 //   credentials: true,
